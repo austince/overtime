37ca01a990f2824e11774c0986b108906363aec3
austin ce <austin.cawley@gmail.com>
2018-05-08 00:14:55 -0400

    Add No reload cmd option

--- core/webpack.dev.js
+++ core/webpack.dev.js
@@ -3,30 +3,39 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const ChromeReloadPlugin = require('wcer')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 const baseWebpack = require('./webpack.base')
 const {styleLoaders} = require('./tools')
 
+const plugins = [
+  new webpack.NoEmitOnErrorsPlugin(),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': '"development"'
+  }),
+  new FriendlyErrorsPlugin(),
 
-module.exports = merge(baseWebpack, {
-  // cheap-module-eval-source-map
-  watch: true,
-  module: {
-    rules: styleLoaders({sourceMap: false})
-  },
-  devtool: '#cheap-module-eval-source-map',
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"development"'
-    }),
-    new FriendlyErrorsPlugin(),
-    new ChromeReloadPlugin({
+  new BundleAnalyzerPlugin({
+    openAnalyzer: false,
+  }),
+]
+
+module.exports = env => {
+  console.log("Reload?", !env || !env['no_reload'])
+
+  if (!env || !env['no_reload']) {
+    plugins.push(new ChromeReloadPlugin({
       port: 9090,
       manifest: path.join(__dirname, '..', 'src', 'manifest.js')
-    }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
-  ]
-})
+    }))
+  }
+
+  return merge(baseWebpack, {
+    // cheap-module-eval-source-map
+    watch: true,
+    module: {
+      rules: styleLoaders({sourceMap: false})
+    },
+    devtool: '#cheap-module-eval-source-map',
+    plugins,
+  })
+}
